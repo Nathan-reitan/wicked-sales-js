3b621cd3bb850b9683f592b3564cfd0cd14a32e3
Nathan Reitan <nathanreitan@gmail.com>
2020-05-26 15:24:21 -0700

    database update

--- database/dump.sql
+++ database/dump.sql
@@ -268,6 +268,15 @@ COPY public."cartItems" ("cartItemId", "cartId", "productId", price) FROM stdin;
 48	29	2	2595
 49	29	3	2900
 50	29	4	999
+51	30	1	99900
+52	31	3	74999
+53	32	3	74999
+54	33	3	74999
+55	33	2	74999
+56	34	2	74999
+57	34	3	74999
+58	35	2	74999
+59	35	3	74999
 \.
 
 
@@ -305,6 +314,12 @@ COPY public.carts ("cartId", "createdAt") FROM stdin;
 27	2020-05-19 13:04:33.199331-07
 28	2020-05-19 13:05:25.267701-07
 29	2020-05-19 13:06:16.494806-07
+30	2020-05-26 13:57:02.534583-07
+31	2020-05-26 13:59:31.317053-07
+32	2020-05-26 14:33:18.089611-07
+33	2020-05-26 14:34:23.482181-07
+34	2020-05-26 14:35:07.810717-07
+35	2020-05-26 14:35:45.307846-07
 \.
 
 
@@ -322,11 +337,11 @@ COPY public.orders ("orderId", "cartId", name, "creditCard", "shippingAddress", 
 
 COPY public.products ("productId", name, price, image, "shortDescription", "longDescription") FROM stdin;
 1	NZXT Starter PC	99900	/images/nzxt-starter.png	A starter PC guaranteed to provide up to 80 FPS running Fortnite	This PC is a great starting PC built in a NZXT H510 White case an Intel Core i5-9400F 6-Core 2.9GHz processor an NVIDIA GeForce GTX 1660 Ti-MSI Ventus XS 6G OC graphics card, an ASRock B365 Phantom Gaming 4 Motherboard, 16GB of Team T-Force Vulcan Z RAM, 1TB of storage, a 500W power supply, Microsoft Windows 10 Operating System, and a 2 year warranty on all parts & labor
-2	IBUYPOWER	74999	/images/iBUYPOWER.jpg	Game to your full potential with this iBUYPOWER gaming desktop computer.	The Intel Core i5 processor and 8GB of RAM multitask smoothly, while the 1TB HDD and a 240GB SSD offer ample file storage space and rapid booting. This iBUYPOWER gaming desktop computer has an NVIDIA GeForce GTX 1650 SUPER graphics card to render detailed visuals for total immersion in your favorite RPG or FPS titles, and the air cooling system keeps components running at optimal temperatures.
-3	ABS Rogue	74999	/images/abs-rogue.jpg	Get a leg up on your competitors with this ABS Rogue	Great budget PC right now thats perfect for upgrades and still has a great punch as is with the RX 580 8gb version which it does not say in the title, and the Ryzen 2600 handles pretty much any game as well, also it does come with 1 stick (1x8gb) so you can add more ram in the 2nd ram slot (motherboard has 2 slots total) once you have the money. The computer also comes with a 512gb ssd with Windows 10 obviously installed to it making the whole pc much faster and zippy whatever you might be doing, after the Windows 10 install I recall having somewhere between 450-480 gb left and it has 2 apps pre-installed and the Newegg webpage on the desktop, now bloatware at all Better than whats in the pictures, the pictures show a 400w power supply which I heard might be kind of pushin it to be enough wattage for the system, and adding more cooling fans (since there is only 1 case and and the cpu fan) would just use up more wattage-but it actually comes with a Raidmax 500w psu which makes me feel a lot safer, plus it comes with a one year warranty.
 4	24" Predator XB1 Gaming Monitor	39999	/images/acer-predator.png	Harness the brilliant colors and vivid display of this Acer Predator monitor.	It displays 16.7 million colors and has a resolution of 1920 x 1080 pixels to provide an incredible viewing experience. This 24-inch LCD Acer Predator monitor with built-in speakers uses LED backlight technology to provide a crisp, clean display.
-5	Logitech G502	4699	/images/logitechG502.jpg	Step up your game with this Logitech HERO Core wired gaming mouse.	This highly customizable device makes it easy to enhance your performance with surface tuning calibration for improved accuracy and 11 programmable buttons for custom commands. This Logitech HERO Core wired gaming mouse lets you change DPI on the fly with three convenient buttons for instant adjustments.
 6	Logitech G910	19999	/images/logitechG910.webp	An amazing Logitech keyboard with RGB capability	Maintain full control of your gaming commands with the customizable, illuminated keys on this Orion Spectrum mechanical gaming keyboard. Connect the unit to a smartphone or tablet to display in-game information when you need a second screen. Anti-ghosting protection keeps the Orion Spark mechanical gaming keyboard from freezing up during intense gaming sessions.
+2	IBUYPOWER	74999	/images/iBUYPOWER.png	Game to your full potential with this iBUYPOWER gaming desktop computer.	The Intel Core i5 processor and 8GB of RAM multitask smoothly, while the 1TB HDD and a 240GB SSD offer ample file storage space and rapid booting. This iBUYPOWER gaming desktop computer has an NVIDIA GeForce GTX 1650 SUPER graphics card to render detailed visuals for total immersion in your favorite RPG or FPS titles, and the air cooling system keeps components running at optimal temperatures.
+5	Logitech G502	4699	/images/logitechG502.png	Step up your game with this Logitech HERO Core wired gaming mouse.	This highly customizable device makes it easy to enhance your performance with surface tuning calibration for improved accuracy and 11 programmable buttons for custom commands. This Logitech HERO Core wired gaming mouse lets you change DPI on the fly with three convenient buttons for instant adjustments.
+3	ABS Rogue	74999	/images/abs-rogue.png	Get a leg up on your competitors with this ABS Rogue	Great budget PC right now thats perfect for upgrades and still has a great punch as is with the RX 580 8gb version which it does not say in the title, and the Ryzen 2600 handles pretty much any game as well, also it does come with 1 stick (1x8gb) so you can add more ram in the 2nd ram slot (motherboard has 2 slots total) once you have the money. The computer also comes with a 512gb ssd with Windows 10 obviously installed to it making the whole pc much faster and zippy whatever you might be doing, after the Windows 10 install I recall having somewhere between 450-480 gb left and it has 2 apps pre-installed and the Newegg webpage on the desktop, now bloatware at all Better than whats in the pictures, the pictures show a 400w power supply which I heard might be kind of pushin it to be enough wattage for the system, and adding more cooling fans (since there is only 1 case and and the cpu fan) would just use up more wattage-but it actually comes with a Raidmax 500w psu which makes me feel a lot safer, plus it comes with a one year warranty.
 \.
 
 
@@ -334,14 +349,14 @@ COPY public.products ("productId", name, price, image, "shortDescription", "long
 -- Name: cartItems_cartItemId_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public."cartItems_cartItemId_seq"', 50, true);
+SELECT pg_catalog.setval('public."cartItems_cartItemId_seq"', 59, true);
 
 
 --
 -- Name: carts_cartId_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public."carts_cartId_seq"', 29, true);
+SELECT pg_catalog.setval('public."carts_cartId_seq"', 35, true);
 
 
 --
